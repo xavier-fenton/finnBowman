22798664dc2730b65d1aa4afc4e1fa05a53184b7
Xavier Fenton <xavierfenton99@gmail.com>
2024-01-20 21:07:05 +1300

    create a random order mosaic hero page

--- src/components/CollectionReactComp.jsx
+++ src/components/CollectionReactComp.jsx
@@ -1,21 +1,42 @@
 import React, { useEffect, useRef } from 'react'
 import data from '../data.json'
+import '../styles/index.css'
 
 const CollectionReactComp = () => {
-  return (
-    <div>
-      <div className="grid grid-cols-2 md:grid-cols-4">
-        {data.map((data) => {
-          return (
-            <div className="grid">
-              <div id={data.id}>
-                <img className="h-auto max-w-full" src={data.img} alt="" />
+  const shuffle = (array) => {
+    return array
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => {
+        return (
+          <div key={a.value.id} id={a.value.id} className="group">
+            <div className="cursor-pointer">
+              <div className="hidden border-4 rounded-lg border-grey-500 bg-black text-white p-2 group-hover:block">
+                <div>{a.value.tag.brand_title}</div>
+                <div>
+                  description:
+                  <div>
+                    Ea sint veniam ea consequat officia fugiat pariatur dolor
+                    consectetur. Reprehenderit tempor consectetur anim ea
+                    exercitation. Laboris ea nisi magna voluptate ad nisi duis.
+                  </div>
+                </div>
               </div>
+              <img className="h-auto" src={a.value.img} alt="" />
             </div>
-          )
-        })}
+          </div>
+        )
+      })
+  }
+  return (
+    <>
+      <div className="grid grid-cols grid-cols-2 md:grid-cols-4">
+        <div className="">{shuffle(data)}</div>
+        <div className="">{shuffle(data)}</div>
+        <div className="">{shuffle(data)}</div>
+        <div className="">{shuffle(data)}</div>
       </div>
-    </div>
+    </>
   )
 }
 
