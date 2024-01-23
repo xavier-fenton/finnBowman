53a99287389ca029d30c547bd746dbc75a97764e
Xavier Fenton <xavierfenton99@gmail.com>
2024-01-23 14:54:02 +1300

    successful sanity intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import { sanityIntegration } from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  output: 'hybrid',
+  integrations: [
+    tailwind(),
+    sanityIntegration({
+      projectId: 'gdu2whhg',
+      dataset: 'production',
+      apiVersion: '2023-02-08',
+      useCdn: false,
+      studioBasePath: '/admin',
+    }),
+    react(),
+  ],
+})
